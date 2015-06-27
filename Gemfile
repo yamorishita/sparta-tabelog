24b97287a5679a005d531b5395414e84370e9f2b
Daisuke Nashiro <nashirox@gmail.com>
2015-06-27 19:29:46 +0900

    facebook認証実装

--- Gemfile
+++ Gemfile
@@ -22,7 +22,8 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 
 gem 'devise'
-gem 'gmaps4rails'
+gem 'omniauth-facebook'
+gem 'omniauth-twitter'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150625101345) do
+ActiveRecord::Schema.define(version: 20150627100728) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string   "name",       null: false
@@ -51,6 +51,8 @@ ActiveRecord::Schema.define(version: 20150625101345) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "uid"
+    t.string   "provider"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
